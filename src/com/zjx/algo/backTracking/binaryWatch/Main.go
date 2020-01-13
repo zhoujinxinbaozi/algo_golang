b13977c73fadc5b2b22bf31d15ff47d298f53872
zhoujinxin <17513158065@163.com>
2020-01-13 10:03:00 +0800

    initital reposity

---
+++ src/com/zjx/algo/backTracking/binaryWatch/Main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/***
+	problem :
+
+	二进制手表顶部有 4 个 LED 代表小时（0-11），底部的 6 个 LED 代表分钟（0-59）。
+	每个 LED 代表一个 0 或 1，最低位在右侧。
+	给定一个非负整数 n 代表当前 LED 亮着的数量，返回所有可能的时间。
+
+	solution:
+
+
+ */
+func main() {
+	s1 := readBinaryWatch(11)
+	fmt.Println(s1)
+}
+
+func readBinaryWatch(num int) []string {
+	var res []string
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 60; j++ {
+			h := fmt.Sprintf("%b", i)
+			m := fmt.Sprintf("%b", j)
+			countOne := strings.Count(h, "1") + strings.Count(m, "1")
+			if countOne == num {
+				res = append(res, fmt.Sprintf("%d:%02d", i, j))
+			}
+		}
+	}
+	return res
+}
