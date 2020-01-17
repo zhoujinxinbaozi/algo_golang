12b72af53ba9799eb9860b59edc8d440c4347ae5
zhoujinxin <17513158065@163.com>
2020-01-17 09:53:43 +0800

    update basic/chan/rangeTraveChan/Main.go

--- src/com/zjx/basic/chan/rangeTraveChan/Main.go
+++ src/com/zjx/basic/chan/rangeTraveChan/Main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	f1()
+}
+
+func f2(){
 	var c1 chan int
 	c1 = make(chan int)
 	go func() {
@@ -14,5 +18,23 @@ func main() {
 
 	for c := range c1 {
 		fmt.Println(c)
+	}
+}
+
+func f1(){
+	var c1 chan int
+	c1 = make(chan int)
+	go func() {
+		for i := 1; i < 100; i++ {
+			c1 <- i
+		}
+		close(c1)
+	}()
+	for{
+		if x, ok := <-c1; ok{ // chan关闭ok为false
+			fmt.Println(x)
+		}else{
+			break
+		}
 	}
 }
