6915ce29d5af478a5a2e513a24e4738a8c6b82e2
JinxinZhou <zhoujinxin@baidu.com>
2021-02-18 15:48:22 +0800

    add

--- src/com/zjx/basic/redis/go-redis_test.go
+++ src/com/zjx/basic/redis/go-redis_test.go
@@ -7,6 +7,7 @@ package redis
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/go-redis/redis"
@@ -19,18 +20,16 @@ var client *redis.Client
 func InitRedis() {
 	client = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379", // redis地址
-		Password: "", // redis密码，没有则留空
-		DB:       0,  // 默认数据库，默认是0
+		Password: "",               // redis密码，没有则留空
+		DB:       0,                // 默认数据库，默认是0
 	})
 }
-
-
 
 // TestGO_Redis
 func TestGO_Redis(t *testing.T) {
 	InitRedis()
 	bytes, err := proto.Marshal(&Person{
-		Name: "杨爽",
+		Name: "123",
 	})
 	if err != nil {
 		fmt.Println("err ", err.Error())
@@ -39,6 +38,7 @@ func TestGO_Redis(t *testing.T) {
 	fmt.Println("bb: ", bytes)
 	HashSet("z", "w", bytes)
 	HashGet("z", "w")
+	HashGetAll("z")
 }
 
 func HashSet(key, field string, val []byte) error {
@@ -55,6 +55,21 @@ func HashGet(key, field string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("result :", r)
+	fmt.Println("result :", strings.TrimLeft(r, "\n"))
+	return nil
+}
+
+// HashGetAll
+func HashGetAll(key string) error {
+	cmd := client.HGetAll(key)
+	m, err := cmd.Result()
+	if err != nil {
+		return err
+	}
+	for i, v := range m {
+		str := fmt.Sprintf("key : %v, value : %v", i, strings.TrimLeft(v,"\n"))
+		fmt.Println(str)
+		fmt.Println(strings.TrimLeft(strings.TrimLeft(v, "\n"), " ") == "123")
+	}
 	return nil
 }
